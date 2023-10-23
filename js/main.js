f3cf455347567bf29d9f0ddf5f443f8c36b3a918
Артур <olmasovarthur@yandex.ru>
2023-10-23 16:35:05 +0500

    Изменил путь файла на относительный, убрал вывод в консоль

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 import './data.js';
-import {createImageStateList} from '/Users/olmasovarthur/Desktop/IT/Обучение IT/HTML Academy/2163157-kekstagram-30/js/data.js';
+import {createImageStateList} from './js/data.js';
 
-console.log(createImageStateList());
+createImageStateList();
